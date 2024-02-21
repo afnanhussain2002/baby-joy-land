3dffdc28485e158d1ba6b97c935ff9416bee7495
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-02-21 10:35:57 +0600

    Build : Install and setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import Header from './sheard/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from './sheard/Footer/Footer';
 
+const App = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
 
-function App() {
-
-
-  return (
-    <>
-     
-    </>
-  )
-}
-
-export default App
+export default App;
